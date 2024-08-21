49bfbfeba49ecc3d464d246547a09a49f50ec8a6
Yuria <yuria.motegi@gmail.com>
2024-08-20 18:05:18 -0700

    Style home about

---
+++ src/components/contents/Home/About.jsx
@@ -0,0 +1,38 @@
+import { Section } from "../../Section";
+
+import imageAbout1 from "../../../assets/img_about_1.png";
+import imageAbout2 from "../../../assets/img_about_2.png";
+import imageAbout3 from "../../../assets/img_about_3.png";
+
+export function About() {
+  return (
+    <Section className="home-about-container" title="About Little Lemon">
+      <p className="text-sm text-desc">
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. The restaurant
+        features a locally-sourced menu with daily specials. Online order and
+        table reservation are available at this website.
+      </p>
+      <div className="image-container">
+        <img
+          className="image-content"
+          width={200}
+          src={imageAbout1}
+          alt="Little Lemon chefs"
+        />
+        <img
+          className="image-content"
+          width={200}
+          src={imageAbout2}
+          alt="Little Lemon chef cooking"
+        />
+        <img
+          className="image-content"
+          width={200}
+          src={imageAbout3}
+          alt="Little Lemon interior"
+        />
+      </div>
+    </Section>
+  );
+}
--- src/components/contents/Home/Home.jsx
+++ src/components/contents/Home/Home.jsx
@@ -1,11 +1,9 @@
-import { Section } from "../../Section";
 import { Hero } from "./Hero";
 import { Specials } from "./Specials";
-
-import imgAbout from "../../../assets/img_about.jpg";
+import { Reviews } from "./Reviews";
+import { About } from "./About";
 
 import "./Home.css";
-import { Reviews } from "./Reviews";
 
 export function Home() {
   return (
@@ -13,22 +11,7 @@ export function Home() {
       <Hero />
       <Specials />
       <Reviews />
-
-      <Section className="home-about-container">
-        <h2>About Little Lemon</h2>
-        <p>
-          Little Lemon is a charming neighborhood bistro that serves simple food
-          and classic cocktails in a lively but casual environment. The
-          restaurant features a locally-sourced menu with daily specials. Online
-          order and table reservation are available at this website.
-        </p>
-        <img
-          width={600}
-          src={imgAbout}
-          alt="About Little Lemon"
-          className="about-image"
-        />
-      </Section>
+      <About />
     </>
   );
 }
